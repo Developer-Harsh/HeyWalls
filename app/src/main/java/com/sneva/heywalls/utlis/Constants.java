43dfa88cb5641de510e4eb0616b3829973a9f444
Harsh Kumar Singh <79015590+Developer-Harsh@users.noreply.github.com>
2023-08-18 20:44:54 +0530

    Update Constants.java

--- app/src/main/java/com/sneva/heywalls/utlis/Constants.java
+++ app/src/main/java/com/sneva/heywalls/utlis/Constants.java
@@ -1,9 +1,9 @@
 package com.sneva.heywalls.utlis;
 
 public class Constants {
-    public static final String FEATURED = "http://s936266511.onlinehome.us/HeyWalls/featured.json";
-    public static final String TABS = "http://s936266511.onlinehome.us/HeyWalls/tabs.json";
-    public static final String WALLPAPERS = "http://s936266511.onlinehome.us/HeyWalls/wallpapers.json";
+    public static final String FEATURED = "http://.../HeyWalls/featured.json";
+    public static final String TABS = "http://.../HeyWalls/tabs.json";
+    public static final String WALLPAPERS = "http://.../HeyWalls/wallpapers.json";
 
     public static final String BANNER = "ca-app-pub-3940256099942544/6300978111";
     public static final String NATIVE = "ca-app-pub-3940256099942544/2247696110";
